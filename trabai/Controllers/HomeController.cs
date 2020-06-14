6b3684165504bc332f6d01908c9ab3f869d7f852
Luis Fernando <luisao96@hotmail.com>
2020-06-14 19:57:03 -0300

    trabalho

---
+++ trabai/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using trabai.Models;
+
+namespace trabai.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        
+        public IActionResult Cadastro()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Cadastro(string nome, string usuário, string email, string senha, bool box)
+        {
+            if (box == false)
+            {
+                ViewData["Error"] = "Da para marcar o termo?";
+            }
+
+            else if (nome != null && usuário != null && email != null && senha != null)
+            {
+                Models.Usuario.ValorUsuario(nome, usuário, email, senha);
+
+                ViewData["Sucess"] = "Agora já era, Cadastrado com sucesso";
+            }
+
+            else
+            {
+                ViewData["Error"] = "Preenche isso certo";
+
+            }
+            return View();
+
+    }
+    }
+}
---
+++ trabai/Models/Usuario.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+
+namespace trabai.Models
+{
+    public class Usuario
+    {
+        public static void ValorUsuario(string nome, string usuário, string email, string senha)
+        {
+            SqlCommand command = new SqlCommand();
+            command.Connection = Conexao.Conectar();
+            command.CommandText = $"INSERT cadastro Values('{nome}','{usuário}','{email}','{senha}')";
+            command.ExecuteNonQuery();
+        }
+     
+    }
+}
